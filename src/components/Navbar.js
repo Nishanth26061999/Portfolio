d97a2209c92a0080913c00e0ba672403b52229b5
aniajith007 <gajith234@gmail.com>
2024-04-28 20:28:28 +0530

    resume onclick bug removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,31 +36,50 @@ const links = [
 // when you click on the nav buttons to scroll down.
 const scrollWidthOffset = (el) => {
     const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-    const yOffset = -80; 
-    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
+    const yOffset = -80;
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
+}
+
+const downloadFile = () => {
+    // Assuming your file is named 'example.txt' and located in the public folder
+    const filePath = '/nishanth_Resume.pdf';
+
+    // Create a link element
+    const link = document.createElement('a');
+    link.href = process.env.PUBLIC_URL + filePath;
+    link.download = 'nishanth_Resume.pdf'; // The name you want to give to the downloaded file
+    document.body.appendChild(link);
+
+    // Trigger the download
+    link.click();
+
+    // Cleanup
+    document.body.removeChild(link);
 }
 
 
 export default function Navbar({ darkMode, handleClick, active, setActive }) {
 
     return (
-        <Box component={'nav'} width={'100%'} position={singlePage ? 'fixed' : 'relative'} className={darkMode? Style.dark : Style.light}>
+        <Box component={'nav'} width={'100%'} position={singlePage ? 'fixed' : 'relative'} className={darkMode ? Style.dark : Style.light}>
             <Box component={'ul'} display={'flex'} justifyContent={'center'} alignItems={'center'}
                 gap={{ xs: '2rem', md: '8rem' }}
-                 fontSize={'1rem'}>
+                fontSize={'1rem'}>
                 {links.map((link, index) => (
                     <Box key={index} component={'li'} className={(link.active === active && !link.type) && Style.active}
                         sx={{ borderImageSource: info.gradient }}>
-                        <Link to={link.name!='Resume' ? `/${link.to}`:`/nishanth_Resume.pdf`} target={link.name=='Resume'} download={link.name=='Resume'}
-                        scroll={el => scrollWidthOffset(el)}
+                        <Link to={link.name != 'Resume' && `/${link.to}`}
+                            scroll={el => scrollWidthOffset(el)}
                             smooth
-                            onClick={() => setActive(link.active)} className={Style.link}>
+                            onClick={() => {
+                                link.name == 'Resume' && downloadFile()
+                                setActive(link.active)
+                            }} className={Style.link}>
                             {!link.type && <p style={{ padding: '0.5rem 0' }}>{link.name}</p>}
                             {link.type && <h1>{link.name}</h1>}
                         </Link>
                     </Box>
                 ))}
-            
                 <li>
                     <Toggler darkMode={darkMode} handleClick={handleClick} />
                 </li>
